79a645798e3859fcec9b7da85734a2d53606b430
Nana Ike <kisaacnana@gmail.com>
2026-08-15 18:16:36 +0000

    feat: add pages CMS workspace

---
+++ src/pages/cms-pages.tsx
@@ -0,0 +1,11 @@
+import CmsSectionPage from './cms-section';
+
+export default function PagesPage() {
+  return (
+    <CmsSectionPage
+      title="Pages"
+      description="Manage public website pages and their publishing state."
+      icon="solar:document-text-bold-duotone"
+    />
+  );
+}
