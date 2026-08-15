98d162ec7c472ac8e3400155882318b0091d9e45
Nana Ike <kisaacnana@gmail.com>
2026-08-15 18:16:44 +0000

    feat: add messages CMS workspace

---
+++ src/pages/cms-messages.tsx
@@ -0,0 +1,11 @@
+import CmsSectionPage from './cms-section';
+
+export default function MessagesPage() {
+  return (
+    <CmsSectionPage
+      title="Messages"
+      description="Review contact submissions and route communications to the appropriate team."
+      icon="solar:letter-bold-duotone"
+    />
+  );
+}
