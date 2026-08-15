5ed0d4e02f0d4d6cd602bcc9c4c758cebab06425
Nana Ike <kisaacnana@gmail.com>
2026-08-15 18:16:55 +0000

    feat: replace template navigation with TYCITC CMS modules

--- src/layouts/config-nav-dashboard.tsx
+++ src/layouts/config-nav-dashboard.tsx
@@ -1,46 +1,16 @@
-import { Label } from 'src/components/label';
 import { SvgColor } from 'src/components/svg-color';
-
-// ----------------------------------------------------------------------
 
 const icon = (name: string) => (
   <SvgColor width="100%" height="100%" src={`/assets/icons/navbar/${name}.svg`} />
 );
 
 export const navData = [
-  {
-    title: 'Dashboard',
-    path: '/',
-    icon: icon('ic-analytics'),
-  },
-  {
-    title: 'User',
-    path: '/user',
-    icon: icon('ic-user'),
-  },
-  {
-    title: 'Gallery',
-    path: '/gallery',
-    icon: icon('ic-cart'),
-    info: (
-      <Label color="error" variant="inverted">
-        *
-      </Label>
-    ),
-  },
-  {
-    title: 'Posts',
-    path: '/posts',
-    icon: icon('ic-blog'),
-  },
-  {
-    title: 'Sign in',
-    path: '/sign-in',
-    icon: icon('ic-lock'),
-  },
-  {
-    title: 'Not found',
-    path: '/404',
-    icon: icon('ic-disabled'),
-  },
+  { title: 'Dashboard', path: '/', icon: icon('ic-analytics') },
+  { title: 'Posts', path: '/posts', icon: icon('ic-blog') },
+  { title: 'Events', path: '/events', icon: icon('ic-calendar') },
+  { title: 'Pages', path: '/pages', icon: icon('ic-file') },
+  { title: 'Media Library', path: '/media', icon: icon('ic-image') },
+  { title: 'Messages', path: '/messages', icon: icon('ic-mail') },
+  { title: 'Users', path: '/users', icon: icon('ic-user') },
+  { title: 'Settings', path: '/settings', icon: icon('ic-settings') },
 ];
