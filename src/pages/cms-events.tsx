f418eb3c4b7d6c36e7359624f93948ebcf800c36
Nana Ike <kisaacnana@gmail.com>
2026-08-15 18:16:30 +0000

    feat: add events CMS workspace

---
+++ src/pages/cms-events.tsx
@@ -0,0 +1,11 @@
+import CmsSectionPage from './cms-section';
+
+export default function EventsPage() {
+  return (
+    <CmsSectionPage
+      title="Events"
+      description="Create, schedule and publish TYCITC events and announcements."
+      icon="solar:calendar-mark-bold-duotone"
+    />
+  );
+}
